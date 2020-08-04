279ff3c7433f96d1d8cdae2d48937c7717c60965
natpauze <nathanieljpauze@gmail.com>
2020-08-03 21:56:50 -0400

    documented the hellow world

--- testing/helloworld.asm
+++ testing/helloworld.asm
@@ -1,13 +1,13 @@
-section .data
+section .data	; data section
 
-    message db "Hello, world!", 10	
+    message db "Hello, world!", 10	; the hello world string and a LF char
 
-section .text
+section .text	 ; test section
 
-global _start
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, message
-    mov rdx, 14
-    syscall
+global _start	; code section 
+_start:			
+    mov rax, 1	; syscall to make, 1 is sys write 
+    mov rdi, 1	; file to print to, 1 is STDOUT
+    mov rsi, message	; mem location of buffer to print 
+    mov rdx, 14		; moves size in bytes of strign to print to rdx 
+    syscall			; does call ( x64 uses syscal isntead of the old interupt on 0x80) 
