dd43a1a3c9b05714e398de05acdf52fabc87265c
natpauze <nathanieljpauze@gmail.com>
2020-08-03 22:00:52 -0400

    used len var

--- testing/helloworld.asm
+++ testing/helloworld.asm
@@ -1,6 +1,7 @@
 section .data	; data section
 
     message db "Hello, world!", 10	; the hello world string and a LF char
+	messageLen db 14
 
 section .text	 ; test section
 
@@ -9,5 +10,5 @@ _start:
     mov rax, 1	; syscall to make, 1 is sys write 
     mov rdi, 1	; file to print to, 1 is STDOUT
     mov rsi, message	; mem location of buffer to print 
-    mov rdx, 14		; moves size in bytes of strign to print to rdx 
+    mov rdx, [messageLen]		; moves size in bytes of strign to print to rdx 
     syscall			; does call ( x64 uses syscal isntead of the old interupt on 0x80) 
