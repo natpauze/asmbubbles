87e9b3252213b59e11533b481c65e290823232c9
natpauze <nathanieljpauze@gmail.com>
2020-08-05 02:46:12 -0400

    prelim implementation of bubble sort. Doesnt work. yay

--- normal_nasm/bubblesort.asm
+++ normal_nasm/bubblesort.asm
@@ -47,18 +47,44 @@ _start:
     mov rsi, [messageLen]
     call print
 
-    mov rdi, testmessage
-    mov rsi, [testmessageLen]
-    call print
+    
 
     ;actual sorting
 
+    ;init
+    mov rax, toSort ;pointer to start of array
+    mov rcx, [toSortLen] ; lenght of the array into outer loop couter
+    mov rdx, [toSortLen] ; lenght of the array into inner loop couter
+    ;mov rdi, 0 ; swap flag 
+
+innerloop:
+    mov bx,[rax] ;load using pointer
+    mov si,[rax+1] ;load using pointer +1
+    cmp bx,si  ;compare them
+    jge dontswap; if greater then other, then we dont swap them 
+    ;but other wise go ahead with swap
+    ;xchg bx,si
+    mov [rax+1],bx 
+    mov [rax], si 
+
+dontswap:
+    inc rax
+    dec rdx  ; decrement inner counter
+    jnz innerloop ; do inner loop iteration again 
+    dec rcx ;decrement outer loop
+    jz _end ; go to end if we are done sorting other wise outer loop makes it go again
+    mov rdx,rcx ;set inner loop back to what it was -1 (-1 allready done)
+    mov rax, toSort;reset pointer to start of array
+    jmp innerloop
 
 
+;go back to the start 
 
+_end:
     mov rdi, toSort
     mov rsi, [toSortLen]
     call print
+
 
 
     			 
