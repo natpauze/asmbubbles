e3635b3ea9684f799124dd916d7cb98d3672e754
natpauze <nathanieljpauze@gmail.com>
2020-08-06 23:05:48 -0400

    made script to assemble without debugging info

---
+++ short_nasm/assemble_nasm_nodebug.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+objfile=$1
+#assemble
+echo Assembling $1 
+nasm -f elf64 $1
+
+#link 
+echo Linking ${objfile/.asm/} 
+ld -m elf_x86_64 -o ${objfile/.asm/} ${objfile/.asm/.o}
+
+#gcc -o -fPIC ${objfile/.asm/} ${objfile/.asm/.o}
